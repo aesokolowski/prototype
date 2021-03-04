97097d3483e0f74e93200e27afe0e27c643ae6c6
Andrew Edward Sokolowski <andysoko1217@gmail.com>
2021-03-04 14:04:05 -0500

    fixed: remembered that I actually have to transpile .ts files myself for mocha to run them, realized that RegExp.test(String) was much more useful to me than String.match(RegExp).

--- src/test/ts/ttt-structs-tests.ts
+++ src/test/ts/ttt-structs-tests.ts
@@ -1,22 +1,38 @@
 const { expect } = require('chai');
-const { xWinConditions, oWinConditions } = require('./test_files/test_tttStructs.ts');
+const { xWinConditions, oWinConditions } = require('./test_files/test_tttStructs');
 
 const simpleTestPatterns = [
-            '---------', 'XXX-O--O-', '-XX-XOO--', 'OX--OXXX0'
+        '---------', 'XXX-O--O-', '-XX-XOO--', 'OX--OXXX0'
 ];
 
 describe('xWinCondtions and oWinConditions:', () => {
     describe('do they pass simple tests?:', () => {
+        let pattern: string, xCondition: RegExp, oCondition: RegExp;
+
         console.log('pattern:', simpleTestPatterns[0]);
         console.log('x condition:', xWinConditions[0]);
         console.log('o condition:', oWinConditions[0]);
         it('first pattern fails first conditon of each:', () => {
-            expect(simpleTestPatterns[0].match(xWinConditions[0])).to.equal(null);
-            expect(simpleTestPatterns[0].match(oWinConditions[0])).to.equal(null);
+            pattern = simpleTestPatterns[0];
+            xCondition = xWinConditions[0];
+            oCondition = oWinConditions[0];
+
+            console.log('1:');
+            console.log('X:', xCondition.test(pattern));
+            expect(xCondition.test(pattern)).to.equal(false);
+            console.log('O:', oCondition.test(pattern));
+            expect(oCondition.test(pattern)).to.equal(false);
         });
-        it('second pattern passes first condition of each:', () => {
-            expect(simpleTestPatterns[1].match(xWinConditions[0])[0]).to.equal(simpleTestPatterns[1]);
-            expect(simpleTestPatterns[1].match(oWinConditions[0])[0]).to.equal(simpleTestPatterns[1]);
+        it('second pattern passes first condition of X but not O:', () => {
+            pattern = simpleTestPatterns[1];
+            xCondition = xWinConditions[0];
+            oCondition = oWinConditions[0];
+
+            console.log('2:');
+            console.log('X:', xCondition.test(pattern));
+            expect(xCondition.test(pattern)).to.equal(true);
+            console.log('O:', oCondition.test(pattern));
+            expect(oCondition.test(pattern)).to.equal(false);
         });
     });
 });
