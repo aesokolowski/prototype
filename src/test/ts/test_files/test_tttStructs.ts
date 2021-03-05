13cda524e2a089c54eff1c651714483521686fa6
Andrew Edward Sokolowski <andysoko1217@gmail.com>
2021-03-05 11:41:47 -0500

    testing: problem I was having with regex turned out to be related to the way I was declaring and using variables in Mocha. I scoped things more strictly and Mocha seems less confused for now.

--- src/test/ts/test_files/test_tttStructs.ts
+++ src/test/ts/test_files/test_tttStructs.ts
@@ -1,28 +1,27 @@
 // winning conditions for X
 const xWinConditions = [
     // across
-    /^XXX[OX-]{6}$/,
-    /^[OX-]{3}XXX[OX-]{3}{$/,
-    /^[OX-]{6}XXX$/,
+    /XXX[OX-]{6}/,
+    /[OX-]{3}XXX[OX-]{3}{/,
+    /[OX-]{6}XXX/,
     // vertical
-    /^X[OX-]{3}X[OX-]{3}X[OX-]{3}$/,
-    /^[OX-]X[OX-]{2}X\[OX-]{2}X[OX-]$/,
-    /^[OX-]{2}X[OX-]{2}X[OX-]{2}X$/,
+    /X[OX-]{3}X[OX-]{3}X[OX-]{3}/,
+    /[OX-]X[OX-]{2}X\[OX-]{2}X[OX-]/,
+    /[OX-]{2}X[OX-]{2}X[OX-]{2}X/,
     // diagonal
-    /^X[OX-]{3}X[OX-]{3}X$/,
-    /^[OX-]{2}X[OX-]X[OX-]X[OX-]{2}$/
-
+    /X[OX-]{3}X[OX-]{3}X/,
+    /[OX-]{2}X[OX-]X[OX-]X[OX-]{2}/
 ];
 
 const oWinConditions = [
-    /^OOO[OX-]{6}$/,
-    /^[OX-]{3}OOO[OX-]{3}$/,
-    /^[OX-]{6}OOO$/,
-    /^O[OX-]{2}O[OX-]{2}O[OX-]{2}$/,
-    /^[OX-]O[OX-]{2}O[OX-]{2}O\[OX-\]$/,
-    /^[OX-]{2}O[OX-]{2}O[OX-]{2}O$/,
-    /^O[OX-]{3}O[OX-]{3}O$/,
-    /^[OX-]{2}O[OX-]O[OX-]O[OX-]{2}$/
+    /OOO[OX-]{6}/,
+    /[OX-]{3}OOO[OX-]{3}/,
+    /[OX-]{6}OOO/,
+    /O[OX-]{2}O[OX-]{2}O[OX-]{2}/,
+    /[OX-]O[OX-]{2}O[OX-]{2}O\[OX-\]/,
+    /[OX-]{2}O[OX-]{2}O[OX-]{2}O/,
+    /O[OX-]{3}O[OX-]{3}O/,
+    /[OX-]{2}O[OX-]O[OX-]O[OX-]{2}/
 ];
 
 module.exports = {
--- src/test/ts/ttt-structs-tests.ts
+++ src/test/ts/ttt-structs-tests.ts
@@ -2,37 +2,35 @@ const { expect } = require('chai');
 const { xWinConditions, oWinConditions } = require('./test_files/test_tttStructs');
 
 const simpleTestPatterns = [
-        '---------', 'XXX-O--O-', '-XX-XOO--', 'OX--OXXX0'
+        '---------', 'XXX-O--O-', '-XX-XOO--', 'OX--OXXXO'
 ];
 
 describe('xWinCondtions and oWinConditions:', () => {
-    let pattern: string, xCondition: RegExp, oCondition: RegExp;
-
     describe('do they pass simple tests?:', () => {
-        xCondition = xWinConditions[0];
-        oCondition = oWinConditions[0];
+        const xCondition: RegExp = xWinConditions[0];
+        const oCondition: RegExp = oWinConditions[0];
 
         console.log('condition 1 (X or O):');
-        console.log('X| | ');
+        console.log('X|X|X');
         console.log('-----');
-        console.log('X| | ');
+        console.log(' | | ');
         console.log('-----');
-        console.log('X| | ');
+        console.log(' | | ');
 
         it('first pattern fails first conditon of each:', () => {
-            pattern = simpleTestPatterns[0];
+            const pattern = simpleTestPatterns[0];
 
             expect(xCondition.test(pattern)).to.equal(false);
             expect(oCondition.test(pattern)).to.equal(false);
         });
         it('second pattern passes first condition of X but not O:', () => {
-            pattern = simpleTestPatterns[1];
+            const pattern = simpleTestPatterns[1];
 
             expect(xCondition.test(pattern)).to.equal(true);
             expect(oCondition.test(pattern)).to.equal(false);
         });
         it('third pattern fails both conditions:', () => {
-            pattern = simpleTestPatterns[2];
+            const pattern = simpleTestPatterns[2];
 
             expect(xCondition.test(pattern)).to.equal(false);
             expect(oCondition.test(pattern)).to.equal(false);
@@ -40,8 +38,8 @@ describe('xWinCondtions and oWinConditions:', () => {
     });
 
     describe('new condition:', () => {
-        xCondition = xWinConditions[6];
-        oCondition = oWinConditions[6];
+        const xCondition: RegExp = xWinConditions[6];
+        const oCondition: RegExp = oWinConditions[6];
 
         console.log('condition 2:');
         console.log('X| | ');
@@ -51,10 +49,22 @@ describe('xWinCondtions and oWinConditions:', () => {
         console.log(' | |X');
 
         it('fourth pattern passes O but not X:', () => {
-            pattern = simpleTestPatterns[3];
+            const pattern = simpleTestPatterns[3];
 
             expect(xCondition.test(pattern)).to.equal(false);
             expect(oCondition.test(pattern)).to.equal(true);
         });
+    });
+
+    describe('all conditions against series of patterns:', () => {
+        const patterns = [ // comments note which condititon pair should get
+                           // satisfied
+            // condition 0            // condition 3
+            'XXX-OO---', 'OOOXX-X--', 'XO-X--XO-', 'OXXOX-O--',
+            // condition 1            // condition 2
+            '-O-XXXO--', 'XXOOOOX-X', 'OOXXOOXXX', 'OXXXX-OOO'
+        ];
+
+        console.log('patterns:', patterns);
     });
 });
