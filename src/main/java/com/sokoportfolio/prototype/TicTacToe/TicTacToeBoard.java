2a62cb90aedfe118d2e3f6dbd06b8c37143e4e7c
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2021-03-12 15:49:31 -0500

    organization: moved TicTacToeServiceImpl.java into the TicTacToe folder on the main java branch; chore: TicTacToeBoard.java is now a proper POJO (overrides equals, hashcode, and toString)

--- src/main/java/com/sokoportfolio/prototype/TicTacToe/TicTacToeBoard.java
+++ src/main/java/com/sokoportfolio/prototype/TicTacToe/TicTacToeBoard.java
@@ -1,5 +1,7 @@
 package com.sokoportfolio.prototype.TicTacToe;
 
+import java.util.Objects;
+
 public class TicTacToeBoard {
     private String board;
     private Character playAs;
@@ -29,5 +31,28 @@ public class TicTacToeBoard {
         this.playAs = playAs;
     }
 
-    // TODO: equals, hashmap, toString
+    // trying out newfangled JDK 7+ way I didn't learn about in college
+    @Override
+    public boolean equals(Object object) {
+        if (object == this)
+            return true;
+        
+        if (object == null || object.getClass() != getClass())
+            return false;
+
+        TicTacToeBoard ticTacToeBoard = (TicTacToeBoard) object;
+        
+        return Objects.equals(board, ticTacToeBoard.board) && Objects.equals(playAs, ticTacToeBoard.playAs);
+    }
+
+    // same as equals, trying out newer "canonical" way instead of old 17/31 way
+    @Override
+    public int hashCode() {
+        return Objects.hash(board, playAs);
+    }
+
+    @Override
+    public String toString() {
+        return "TicTacToeBoard{board=\"" + board + "\",playAs='" + playAs + "'}";
+    }
 }
